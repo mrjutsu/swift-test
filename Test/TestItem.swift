cc2b22312d4d99d834cd59e045abd20a31eb6684
David Rosillo Ricardo <drr538@gmail.com>
2016-09-03 11:12:43 -0500

    Use NSCoding

---
+++ Test/TestItem.swift
@@ -0,0 +1,44 @@
+//
+//  TestItem.swift
+//  Test
+//
+//  Created by David Rosillo Ricardo on 3/09/16.
+//  Copyright © 2016 David Rosillo Ricardo. All rights reserved.
+//
+
+import UIKit
+
+class TestItem: NSObject, NSCoding {
+    
+    var todo: String?
+    
+    var dueDate: NSDate?
+    
+    var image: UIImage?
+    
+    required init(coder aDecoder: NSCoder){
+        super.init()
+        if let message = aDecoder.decodeObjectForKey("todo") as? String {
+            self.todo = message
+        }
+        if let date = aDecoder.decodeObjectForKey("dueDate") as? NSDate {
+            self.dueDate = date
+        }
+        if let img = aDecoder.decodeObjectForKey("image") as? UIImage {
+            self.image = img
+        }
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        if let message = self.todo {
+            aCoder.encodeObject(message, forKey: "todo")
+        }
+        if let date = self.dueDate {
+            aCoder.encodeObject(date, forKey: "dueDate")
+        }
+        if let img = self.image {
+            aCoder.encodeObject(img, forKey: "image")
+        }
+    }
+    
+}
